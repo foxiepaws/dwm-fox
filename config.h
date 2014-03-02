2caee3e2bb2ad0f98d5443290139608e7d676fbf
Allison Rachel Fox <fox@foxiepa.ws>
2014-03-02 10:39:56 -0500

    changed font from terminus to dina
    changed the font from a terminus xlft string to dina
    using fontconfig.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
+static const char font[]            = "Dina-9:Regular";
 static const char normbordercolor[] = "#3f3f3f";
 static const char normbgcolor[]     = "#3f3f3f";
 static const char normfgcolor[]     = "#dcdccc";
